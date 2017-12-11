8da68f75fd16f9cc118422abb2a04a9fba9a0133
Luke <weberluke3219@gmail.com>
2017-12-11 14:57:54 +1300

    Local Storage Setup
    Added a local storage function so that any site name/url submited will be stored in an JSON array object in the user's local storage.
    
    Also added an onload function called "fetchBookmarks" to fetch & display all the current bookmarks.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,54 @@
- //add an event listener to the submit button of the myForm
+ //add an event listener to the submit button of myForm
  document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
-//function to get the values of form input
+// Function to get the values of form input and store the
+// bookmark in local storage as an object array
  function saveBookmark(e){
    //define form input variables
    var siteName = document.getElementById('siteName').value;
    var siteUrl = document.getElementById('url').value;
 
-   //define bookmark object with input variables as parameters
-   var bookmark = {
+   //define bookmark object
+   var newBookmark = {
      name: siteName,
      url: siteUrl
    }
 
-   // Store a string into local storage...
-
-
+   // Local Storage only stores strings.
+   // Have to parse JSON object and store as strings
+   // Test if bookmark is null, else add to the list
+   if(localStorage.getItem('bookmarks') === null){
+      // initialize bookmark array
+      var bookmarksArray = [];
+      bookmarksArray.push(newBookmark);
+      // Parse bookmark object to a string and set to local storage with
+      // key value of "bookmarks"
+      localStorage.setItem('bookmarks', JSON.stringify(bookmarksArray));
+    } else {
+      // Get current bookmarks as a JSON from local storage
+      // store all the values in a new array called currentBookmarks
+       var currentBookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+      //add bookmark the new bookmark
+      currentBookmarks.push(newBookmark);
+      //reset to local storage
+      localStorage.setItem('bookmarks', JSON.stringify(currentBookmarks));
+      }
+   // Prevent form from submitting
    e.preventDefault();
  }
+
+
+// Function to fetch all the bookmarks from local Storage
+// and display them to the user
+function fetchBookmarks(){
+  //get all the curretn bookmarks and store in a local variable
+  var displayBookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  // build the bookmarks in the results div
+  var resultsDiv = document.getElementById("bookmarkResults");
+  resultsDiv.innerHTML = '';
+
+  if(displayBookmarks === null) {
+    resultsDiv.innerHTML = 'Bookmarks you type will be displayed here...';
+  }
+
+}
