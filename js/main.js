f5cf031f6c219b9b0f77615ebd849fd5d071c11c
Luke <weberluke3219@gmail.com>
2017-12-13 13:13:39 +1300

    Finsihing touches
    1) Defined display case when there are no bookmarks in local storage
    
    2) added function to delete button so that when it is clicked the bookmark object is removed from local storage and the bookmark display is regenerated without that bookmark.
    
    3) Added 2 form validations before submit can be clicked (empty fields and valid url path including https://) in a function called formValidate()
    
    4) Reset the form fields with .reset() each time submit is clicked.

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@
    var siteName = document.getElementById('siteName').value;
    var siteUrl = document.getElementById('url').value;
 
+   if(!formValidation(siteName, siteUrl)){
+      return false;
+   }
+
    //define bookmark object
    var newBookmark = {
      name: siteName,
@@ -33,22 +37,30 @@
       //reset to local storage
       localStorage.setItem('bookmarks', JSON.stringify(currentBookmarks));
       }
+
+      //reset the form fields upon each submit. 
+      document.getElementById('myForm').reset();
+
+    // display all bookmarks below submit button. Function defined below.
+    fetchBookmarks();
    // Prevent form from submitting
    e.preventDefault();
+
  }
 
 
 // Function to fetch all the bookmarks from local Storage
 // and display them to the user
 function fetchBookmarks(){
-  //if there are no bookmarks
-  if(displayBookmarks === null || displayBookmarks == null){
+  //get all the current bookmarks and store in a local variable
+  var displayBookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  //check if there are no bookmarks
+  if(displayBookmarks === null || displayBookmarks.length == 0){
     console.log("display bookmarks is null");
     document.getElementById("bookmarkResults").innerHTML = "Your bookmarks will display here...";
     return;
   } else {
-    //get all the current bookmarks and store in a local variable
-    var displayBookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    console.log("display bookmarks var is NOT null!");
     // // build the bookmarks in the results div
     var resultsDiv = document.getElementById("bookmarkResults");
     resultsDiv.innerHTML = '';
@@ -63,15 +75,46 @@ function fetchBookmarks(){
       resultsDiv.innerHTML += '<div class="well">' +
                               '<h3>' +siteName+
                               ' <a class="btn btn-default" target="_blank" href="'+siteUrl+'">Visit</a> ' +
-                              ' <a onclick="deleteBookmark(\''+url+'\')"class="btn btn-danger" target="_blank" href="#">Delete</a> '
+                              ' <a onclick="deleteBookmark(\''+siteUrl+'\')" class="btn btn-danger" href="#">Delete</a> '
                               '</h3>' +
                               '</div>';
      }
   }
 }
 
+// function to delete the bookmark from local Storage
+// will run when the delete button is clicked
+function deleteBookmark(url){
+  //get all the bookmarks in local Storage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  //loop through all the bookmarks
+  for(var i=0;i<bookmarks.length;i++){
+    if(bookmarks[i].url == url){
+      //remove from bookmarks
+      bookmarks.splice(i, 1);
+    }
+  }
+  //reset local Storage
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 
-// function deleteBookmark(){
-//   // this function will delete a bookmark from local storage
-//   // when the delete button is clicked.
-// }
+  //refetch bookmarks so that the displayed list is updated
+  fetchBookmarks();
+}
+
+//functiion for validating the form fields
+function formValidation(siteName, siteUrl){
+  //form validation if empty
+  if(siteName == '' || siteUrl == ''){
+    alert('Please fill in the form before clicking submit');
+    return false;
+  }
+  //form validation regexp
+  var expression = /https:[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;;
+  var regex = new RegExp(expression);
+
+  if(!siteUrl.match(regex)){
+     alert('Please use the valid url https://www.example.com');
+     return false;
+  }
+  return true;
+}
